1613ae42fac1c7f275361803b3b66cf6d904e2f0
biaag10 <107511894+biaag10@users.noreply.github.com>
2025-03-26 14:49:17 -0300

    resolveu o erro do objeto nao conseguir ser exibido pelo react

--- src/components/DisplayIdade.jsx
+++ src/components/DisplayIdade.jsx
@@ -1,32 +1,33 @@
-import { useSpring, animated } from "@react-spring/web";
-import PropTypes from "prop-types";
-
-// faz a mudança dos valores, atualização doa dados
+import { useSpring, animated } from "@react-spring/web";  // Importando a biblioteca de animação
+import PropTypes from "prop-types";  // Importando PropTypes para validação de propriedades
 
 const DisplayIdade = ({ label, value }) => {
-
-  // se valor for zero exibe o "--"
   const ehZero = value === 0;
 
-  // useSpring cria uma animação para o número de 0 até o valor fornecido
-  const { number } = useSpring({ number: value, from: { number: 0 } });
+  // usa useSpring para criar uma animação do número
+  // resolver o erro para conseguir mostrar o objeto em tela, react so recebe os numeros para exibir com isso
+  const { number } = useSpring({
+    number: value,  // valor final da animação
+    from: { number: 0 },  // valor inicial (começa do zero)
+    config: { tension: 200, friction: 20 },  // Ccnfiguração da animação
+  });
 
   return (
     <h2>
       {ehZero ? (
-        <span className="text-purple">--</span>
+        <span className="text-purple">--</span>  // Exibe '--' se o valor for zero
       ) : (
         <animated.span className="text-purple">
-          {number.to((n) => Math.floor(n))} {/* arredonda o número para inteiro */}
+          {number.to((n) => Math.floor(n))}  
         </animated.span>
-
-      )}{" "} {/* espaço entre o número e o label */}
-      {label}
+      )}
+      {" "}
+      {label}  
     </h2>
   );
 };
 
-// definição das propTypes
+// Definição das propTypes
 DisplayIdade.propTypes = {
   label: PropTypes.string.isRequired,
   value: PropTypes.number.isRequired,
